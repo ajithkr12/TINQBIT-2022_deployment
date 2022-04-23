3c3d16154eeb5fe6b2fb3d76eb94312f14644a12
Ajith K.R <46483037+ajithkr12@users.noreply.github.com>
2022-04-23 20:16:46 +0530

    Add files via upload

---
+++ precache-manifest.35814edbbf5772bfd8d87cee86ec972f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7053144d656d833f974f3610afc80b20",
+    "url": "/index.html"
+  },
+  {
+    "revision": "886c6bf741bf13f7b515",
+    "url": "/static/css/main.2763d104.chunk.css"
+  },
+  {
+    "revision": "0752c02ac5b2359f253d",
+    "url": "/static/js/2.078d72b6.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.078d72b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "886c6bf741bf13f7b515",
+    "url": "/static/js/main.aa461def.chunk.js"
+  },
+  {
+    "revision": "476fec442afe5b76bad1",
+    "url": "/static/js/runtime-main.0e51c17f.js"
+  },
+  {
+    "revision": "add73d0f5a31425ab8d0cb828160bd50",
+    "url": "/static/media/about.add73d0f.jpg"
+  },
+  {
+    "revision": "68f488aacb1dde4046b5c9e810042fc4",
+    "url": "/static/media/camera.68f488aa.jpg"
+  },
+  {
+    "revision": "dc265d8fd01dadb6e777c3ab310a3933",
+    "url": "/static/media/keyboard.dc265d8f.jpg"
+  },
+  {
+    "revision": "9a500e30a0b5ec28c8afe061ba082c92",
+    "url": "/static/media/logo_dark.9a500e30.png"
+  }
+]);
